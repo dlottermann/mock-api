cb40a14f8db42891f25f2d4462ba1afbf8375b89
dlottermann <diegolottermann@gmail.com>
2020-08-19 16:21:43 -0300

    fix: First Name and Last name on schema

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const passwd = process.env.PASSWD
 
 const authorSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    firstname: String,
-    lastname: String,
+    firstName: String,
+    lastName: String,
     created: { 
       type: Date,
       default: Date.now
