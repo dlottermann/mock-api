86957ac1efddfd530b573b6bd959cd569f3f2967
dlottermann <diegolottermann@gmail.com>
2020-08-19 15:53:30 -0300

    changes: Add mongo connection

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
+
 const port = process.env.PORT
+const dbname = process.env.DBNAME
+const userdb = process.env.USERDB
+const passwd = process.env.PASSWD
+
+
+
+const uri = `mongodb+srv://${userdb}:${passwd}@supermarketapp.gyn7i.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+
+const connection = mongoose.connection;
+
+connection.once("open", function() {
+  console.log("MongoDB database connection established successfully");
+});
+
 
 
 app.get('/', function(req, res) {
-    res.send('Olá Mundo!');
+    res.send('Olá Mundo connectado!');
   });
   
   app.listen(port, function() {
